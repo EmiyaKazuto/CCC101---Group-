5c30433fc8087edd69e343a6da8ba0b1ce06afd8
Shaina Uy <shaina.uy@msugensan.edu.ph>
2021-06-29 02:15:25 +0800

    Update calcu_logic.py

--- calcu_logic.py
+++ calcu_logic.py
@@ -1,9 +1,6 @@
 from math import *
 
 sin, cos, tan = math.sin, math.cos, math.tan
-from math import *
-
-sin, cos, tan = math.sin, math.cos, math.tan
 sqrt = math.sqrt
 power = math.pow
 π = math.pi
@@ -122,96 +119,4 @@ class Calculator():
         self.prev_ans = self.current_expression[:]
         self.total_expression = ''
         self.update_label()
-
-
-    def append_operator(self, operator):
-        self.current_expression += operator
-        self.total_expression += self.current_expression
-        self.current_expression = ""
-
-    def add_to_expression(self, value):
-        self.current_expression += str(value)
-    
-    def log_function(self):
-        self.current_expression = 'log(' + self.current_expression
-
-    def ln_function(self):
-        self.current_expression = 'ln(' + self.current_expression
-
-    def abs_function(self):
-        self.current_expression = 'abs(' + self.current_expression
-
-    def trig_sine(self):
-        self.current_expression =  'sin(' + self.current_expression
-
-    def trig_cosine(self):
-        self.current_expression =  'cos(' + self.current_expression
-
-    def trig_tangent(self):
-        self.current_expression =  'tan(' + self.current_expression
-
-    def squared(self):
-        self.current_expression = self. current_expression + '²'
-
-    def sqrt(self):
-        self.current_expression = 'sqrt(' + self.current_expression
-
-    def cubed(self):
-        self.current_expression = self. current_expression + '³'
-
-    def perm(self):
-        self.current_expression = self.current_expression + 'P'
-
-    def comb(self):
-        self.current_expression = self.current_expression + 'C'
-
-    def clear(self):
-        self.current_expression = ""
-        self.total_expression = ""
-
-    def delete(self):
-        self.current_expression = str(self.current_expression[:-1])
-
-    def answer(self):
-        self.answer = str(self.total_expression)
-        self.current_expression = self.current_expression + self.answer
-
-    def exp_function(self):
-        self.current_expression = self.current_expression + 'E'
-
-    def evaluate(self):
-        self.total_expression += self.current_expression
-
-        if '²' in self.total_expression or '³' in self.total_expression or 'E' in self.total_expression:
-            self.current_expression = str(eval(self.total_expression.replace('²', '**2').replace('³', '**3').replace('E','*10**')))
-        
-        elif 'P' in self.total_expression or 'C' in self.total_expression:
-            pc = []
-            x = self.total_expression
-            for i in range(len(x)):
-                if x[i] == 'P' or x[i] == 'C':
-                    pc.append(x[i-1] + x[i] + x[i+1])
-            for term in pc:
-                if 'P' in term:
-                    n,r = term.split('P')
-                    n,r = int(n), int(r)
-                    if 0 <= r <= n: 
-                        x = x.replace(term, f'math.factorial({n})' + '//' + f'math.factorial({n}-{r})')
-                elif 'C' in term:
-                    n,r = term.split('C')
-                    n,r = int(n), int(r)
-                    if 0 <= r <= n:
-                        x = x.replace(term, f'math.factorial({n})' + '//' + f'math.factorial({r})' + '//' + f'math.factorial({n}-{r})' )
-            self.current_expression = str(eval(x))
-        else:
-            try:
-                self.current_expression = str(eval(self.total_expression))
-                self.total_expression = ""
-            except Exception as e:
-                self.current_expression = "Error"
-""""
-TO DO:
-1. Proper Error Handling
-2. Test Files
-"""
 
