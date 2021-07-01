78f16caec67da8fbaaec47fe4ce950a05ea03cc6
Shaina Uy <shaina.uy@msugensan.edu.ph>
2021-07-01 17:20:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -164,4 +164,136 @@ class TestCalculator(unittest.TestCase):
 
         
 if __name__ == "__main__":
-	unittest.main()     
+	unittest.main() 
+	
+"""
+## RAN 8 TESTS - OK
+import unittest
+import math
+from logic import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add_to_expression(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(2)
+        c.add_to_expression(3)
+        # Test if the current expression matches the combination of the inputted numbers
+        self.assertEqual(c.current_expression, '123')
+
+    def test_append_operator(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(2)
+        c.add_to_expression(3)
+        # Append an operator to the expression
+        c.append_operator('+')
+        # Test if the operator was appended to the total expression
+        self.assertEqual(c.total_expression, '123+')
+        # Test if the current expression is cleared
+        self.assertEqual(c.current_expression, '')
+
+    def test_evaluate_expression_add(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        # Append an operator to the expression
+        c.append_operator('+')
+        # Add additional numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        c.evaluate()
+        # Test if the calculator evaluates the expression correctly
+        self.assertEqual(c.current_expression, '20')
+
+    def test_evaluate_expression_subtract(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        # Append an operator to the expression
+        c.append_operator('-')
+        # Add additional numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        c.evaluate()
+        # Test if the calculator evaluates the expression correctly
+        self.assertEqual(c.current_expression, '0')
+    
+    def test_evaluate_expression_Multiply(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        # Append an operator to the expression
+        c.append_operator('*')
+        # Add additional numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        c.evaluate()
+        # Test if the calculator evaluates the expression correctly
+        self.assertEqual(c.current_expression, '100')
+
+    def test_evaluate_expression_Divide(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        # Append an operator to the expression
+        c.append_operator('/')
+        # Add additional numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        c.evaluate()
+        # Test if the calculator evaluates the expression correctly
+        self.assertEqual(c.current_expression, '1.0')
+    
+    def test_evaluate_expression_add_and_subtract(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        c.add_to_expression(0)
+        # Append an operator to the expression
+        c.append_operator('+')
+        # Add additional numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        # Append an operator to the expression
+        c.append_operator('-')
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(2)
+        c.add_to_expression(0)
+        # Test if the calculator evaluates the expression correctly
+        c.evaluate()
+        self.assertEqual(c.current_expression, '-10')
+
+    def test_square_a_number(self):
+
+        c = Calculator()
+        # Add some numbers to the current expression
+        c.add_to_expression(1)
+        c.add_to_expression(0)
+        # Square the inputted number
+        c.squared()
+        c.evaluate()
+        # Test if the calculator gives the correct result
+        self.assertEqual(c.current_expression, '100')
+
+
+if __name__ == "__main__":
+    unittest.main()
+"""
